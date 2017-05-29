32079cdfe413703725ea1e935372618cd826a48f
Artiom Chilaru <artiom@flexlabs.org>
2017-05-29 20:59:13 +0100

    Moving away from .tt generation - Removing .tt files and the generated files + Adding some of the extensions from R4MVC code + Adding the MvcActionTagHelper

---
+++ src/T4MVCCoreLite/TagHelpers/MvcActionTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Microsoft.AspNetCore.Mvc.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = ActionAttribute)]
+    public class MvcActionTagHelper : TagHelper
+    {
+        private const string ActionAttribute = "mvc-action";
+
+        private readonly IUrlHelperFactory _urlHelperFactory;
+        public MvcActionTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelperFactory = urlHelperFactory;
+        }
+
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+        [HtmlAttributeName(ActionAttribute)]
+        public IActionResult Action { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll(ActionAttribute);
+            var urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext);
+
+            if (Action is IT4MVCCoreActionResult t4mvcActionResult)
+            {
+                var url = urlHelper.RouteUrl(t4mvcActionResult.RouteValueDictionary);
+                output.Attributes.SetAttribute("href", url);
+            }
+        }
+    }
+}
---
+++ src/T4MVCCoreLiteTool/StaticFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using T4MVCCoreLiteTool.Extensions;
+
+namespace T4MVCCoreLiteTool
+{
+    public class StaticFile
+    {
+        public StaticFile(string fileName, Uri relativePath, Uri collectionName)
+        {
+            FileName = fileName.Replace(new[] { '.', '-', ' ' }, "_");
+            RelativePath = relativePath;
+            CollectionName = collectionName;
+        }
+
+        public string FileName { get; set; }
+
+        public Uri RelativePath { get; set; }
+
+        public Uri CollectionName { get; set; }
+    }
+}
