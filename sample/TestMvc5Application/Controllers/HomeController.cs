def2836c4c94d6756b8fe60ff625fc54d2edb455
curlysemi <julian@juliangeiger.com>
2018-12-06 12:29:36 -0700

    Add TestMvc5Application

---
+++ sample/TestMvc5Application/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMvc5Application.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        public virtual ActionResult Index()
+        {
+            return View(MVC.Home.Views.Index);
+        }
+
+        public virtual ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View(MVC.Home.Views.About);
+        }
+
+        public virtual ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View(MVC.Home.Views.Contact);
+        }
+    }
+}
---
+++ sample/TestMvc5Application/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestMvc5Application.MvcApplication" Language="C#" %>
